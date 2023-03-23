ea32ec003633749443cb19b1975ee136c62c98d9
amir-ys <amir.you74@gmail.com>
2023-03-23 18:00:52 +0430

    Autoloading class

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 session_start();
 const DISPLAY_ERROR = true;
-const BASE_PATH = __DIR__ ;
+const BASE_PATH = __DIR__;
 
 require 'core/helpers/helper.php';
+
+spl_autoload_register(function ($class) {
+    require base_path("core/$class" . ".php");
+});
+
 require base_path('core/config/error.php');
-require base_path('core/Response.php');
-require base_path('core/Validator.php');
-require base_path('core/database/Database.php');
 require base_path('core/route/router.php');
