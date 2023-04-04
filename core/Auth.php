ce53d29a773baeb0d9f82f0fb2666feb6232b167
amir-ys <amir.you74@gmail.com>
2023-04-05 00:12:31 +0430

    add Auth class for authenticate user

---
+++ core/Auth.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Core;
+
+
+use App\Models\User;
+
+class Auth
+{
+    public static function user()
+    {
+        if (is_null($_SESSION['user'])) {
+            return null;
+        }
+
+        $user = (new User())->find($_SESSION['user']);
+
+        return $user ?: null;
+    }
+
+    public static function login($user)
+    {
+        if (!$user && !($user instanceof User)) {
+            throw new \Exception("argument of login method must be instead of " . User::class);
+        }
+
+        $_SESSION['user'] = $user->id;
+
+    }
+
+    public static function loginUsingId(int $id)
+    {
+        $user = (new User())->find($id);
+        static::login($user);
+    }
+
+}
